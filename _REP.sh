085af94ec58d6e933c83b6ba2642f8f9a73a2c03
devg1120 <devg1120@gmail.com>
2025-06-23 08:06:46 +0000

    first commit

---
+++ _REP.sh
@@ -0,0 +1,9 @@
+
+TARGET_DIR="./"
+
+#BEFORE=\"\.\./\.\./\.\./\.\./\.\./dist/mxgraph\.es\.js\"
+#AFTER=\"\.\./\.\./\.\./\.\./dist/mxgraph\.es\.js\"
+BEFORE="\"\.\.\/"
+AFTER="\""
+find ${TARGET_DIR}  -type f
+find ${TARGET_DIR}  -type f | xargs sed -i "s/${BEFORE}/${AFTER}/g"
